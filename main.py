0b2ab6f35b67a88b6bea5f74af4ba943dc3c03c9
Ian Hales <contact@ian-hales.com>
2021-01-30 14:28:47 +0000

    Added CORS passthrough to the app. Origins are defined in the config file.

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from typing import Dict
+from typing import Dict, List
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import under_control
+import under_control.config as config
+import under_control.logger as log
 
 app = FastAPI()
+
+
+def set_cors(app: FastAPI, origins: List[str]):
+    log.logger.info(f"Enabling CORS middleware for origins: {origins}")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 @app.get("/")
@@ -20,5 +34,8 @@ def shutdown_event():
 
 if __name__ == "__main__":
     under_control.setup('config.toml')
+    cors_origins = config.get("cors_origins")
+    if cors_origins:
+        set_cors(app, cors_origins)
     under_control.start(app)
     uvicorn.run(app, host="0.0.0.0", port=7654)
